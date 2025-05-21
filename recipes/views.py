6f0f212f62c758be965212366b5d01958d0b285e
Flavimm <flavionegocios2004@gmail.com>
2025-05-20 23:22:42 -0300

    Templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home 1")
+    return render(request, 'recipes/home.html', status=201, context={
+        'name': 'Flavio Costa',
+        'age':21,
+    })
 
 def contato(request):
     return HttpResponse("Contato 1")
