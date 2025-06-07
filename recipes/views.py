79695acd7f69fca12269f587cba7965f0eca7e7b
Flavimm <flavionegocios2004@gmail.com>
2025-06-07 11:43:51 -0300

    7/6/2025

--- recipes/views.py
+++ recipes/views.py
@@ -3,8 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/pages/home.html', status=201, context={
-        'name': 'Flavio Costa',
-        'age':21,
-    })
+    return render(request, 'recipes/pages/home.html', status=201)
 
