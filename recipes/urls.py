40045cedfbf34027e52a9b8f01b312b569280c55
Flavimm <flavionegocios2004@gmail.com>
2025-06-08 14:28:52 -0300

    Aula 50 - 51 | Criação de tabela(Models) , migrate e makemigrate

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,8 +4,10 @@ from . import views
 # Request = Cliente faz
 # Response = Servidor responde
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home), #Home
-    path('recipes/<int:id>/', views.recipe), #Recipe
+    path('', views.home, name='home'), #Home
+    path('recipes/<int:id>/', views.recipe, name='recipe'), #Recipe
    
 ]
