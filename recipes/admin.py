249c4ea8a1e3a45c03048dfed28742338e7d55fe
Flavimm <flavionegocios2004@gmail.com>
2025-06-13 22:37:46 -0300

    media

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
 # Register your models here.
+
+
+# 1º maneira de registrar o model 
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+# 2º maneira de registrar o model 
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
